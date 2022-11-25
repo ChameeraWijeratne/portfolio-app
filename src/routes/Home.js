ee4a0b4c57f576af4afb43eabe7d88dae527924f
Chameera <wijerat>
2022-11-26 01:17:14 +0530

    5

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from "../Components/Navbar";
 import Img from "../Components/Img";
+import Footer from "../Components/Footer";
 
 const Home = () =>{
     return <div>
         <Navbar/>
         <Img/>
+        <Footer/>
     </div>
 
-}
+};
 
-export default Home
+export default Home;
