0ea78e9fb6726d908041fcb234588bd3fffc8e1c
Chameera <wijerat>
2022-11-26 13:24:02 +0530

    7

---
+++ src/Components/WorkCard.js
@@ -0,0 +1,32 @@
+import "./WorkCardStyles.css"
+
+import React from "react";
+import pro1 from "../assets/bg-pro1.png"
+import {NavLink} from "react-router-dom";
+
+const WorkCard = () => {
+  return (
+      <div className="work-container">
+          <h1 className="project-heading">
+              Projects
+          </h1>
+          <div className="project-container">
+              <div className="project-card">
+                  <img src={pro1} alt="image"/>
+                  <h2 className="project-title">
+                      Project Title
+                  </h2>
+                  <div className="pro-details">
+                      <p>This is text</p>
+                      <div className="pro-btns">
+                          <NavLink to="url.com" className="btn">VIEW</NavLink>
+                          <NavLink to="url.com" className="btn">SOURCE</NavLink>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+};
+
+export default WorkCard;
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import Img2 from "../Components/Img2";
+import PricingCard from "../Components/PricingCard";
+import WorkCard from "../Components/WorkCard";
 
 const Project = () =>{
-    return <div>
+    return (<div>
         <Navbar/>
         <Img2 heading="PROJECTS." text="Some of my most recent works"/>
+        <WorkCard/>
+        <PricingCard/>
         <Footer/>
     </div>
-}
+    );
+};
 
-export default Project
+export default Project;
